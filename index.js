7f97c46758f3bb75f0d319b47741fa75a50673ab
Jonathan Irvin <jonathan.irvin@harlandclarke.com>
2018-07-17 15:34:45 -0500

    feat: got vehicle crud working

--- index.js
+++ index.js
@@ -1,40 +1,81 @@
-const express = require('express');
-const bluebird = require('bluebird');
-
-// https://www.npmjs.com/package/sqlite3
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+const express = require("express");
+const nSQL = require("nano-sql").nSQL;
+const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
- 
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
+var lastAdded = 0;
+
+const eventFn = (event, db) => {
+  //console.log(event);
+  lastAdded = event.affectedRowPKS[0];
+}
+
+nSQL("vehicles")
+  .model([
+    { key: "id", type: "int", props: ["pk", "ai"] },
+    { key: "type", type: "string" },
+    { key: "props", type: "any" }
+  ])
+  .config({
+    mode: "PERM", // store changes permenantly
+    history: "row" // store each row's changes as a revision history
+  })
+  .connect()
+  .then(() => {
+    // Database is now ready to use.
+
+    // put some data in
+    // nSQL("posts").query("upsert", {id: 1, title: "Hello World!", content: "This is my first post!", date: Date.now()}).exec();
+
+    nSQL("vehicles").on("upsert", eventFn);
+
+    app.post("/vehicles/add/:type", (req, res) => {
+      
+      nSQL("vehicles")
+        .query("upsert", {
+          type: req.params.type,
+          props: JSON.stringify(req.body)
+        })
+        .exec();
+      res.send(req.params.type + " added");      
+    });
+
+    app.get("/vehicles/all", (req, res) => {
+      nSQL("vehicles")
+        .query("select")
+        .exec()
+        .then(rows => res.send(rows));
+    });
+
+    app.get("/vehicles/:id", (req, res) => {
+      nSQL("vehicles")
+        .query("select")
+        .where(["id", "=", req.params.id])
+        .exec()
+        .then(rows => res.send(rows));
+    });    
+
+    app.delete("/vehicles/last", (req, res) => {
+      if(lastAdded === 0){
+        res.send("Haven't added any vehicles recently!")
+      }else{
+        nSQL("vehicles")
+        .query("delete")
+        .where(["id", "=", lastAdded])
+        .exec()
+        .then(res.send(lastAdded + " deleted"));
+      }
+    });
+
+    app.delete("/vehicles/:id", (req, res) => {
+      nSQL("vehicles")
+        .query("delete")
+        .where(["id", "=", req.params.id])
+        .exec()
+        .then(res.send(req.params.id + " deleted"));
+    });
+
+    app.listen(3000, () => console.log("Vehicle lite listening on port 3000!"));
   });
-});
- 
-db.close();
- 
-app.get('/post/:id', async (req, res, next) => {
-  try {
-    const db = await dbPromise;
-    const [post, categories] = await Promise.all([
-      db.get('SELECT * FROM Post WHERE id = ?', req.params.id),
-      db.all('SELECT * FROM Category')
-    ]);
-    res.render('post', { post, categories });
-  } catch (err) {
-    next(err);
-  }
-});
- 
-app.listen(port);
